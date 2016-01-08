9663edf1d923c50fedb7fb35111b6e0b2d93f492
Jim Vallandingham <vlandham@gmail.com>
2016-01-07 22:15:26 -0800

    Unbreak tests

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -10,6 +10,8 @@ def test_lowercase():
         with open(filename, 'w') as f:
             f.write('Hello\nWorld\n!\nI\n.\nnoooo\n')
 
-        # result = runner.invoke(lowercase, [filename])
+        result = runner.invoke(lowercase, [filename])
 
-        # assert result.exit_code == 0
+        assert result.exit_code == 0
+        assert result.output.split('\n')[0] == 'hello'
+        assert result.output.split('\n')[1] == 'world'
