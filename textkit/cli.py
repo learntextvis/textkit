9577a5a5fea33472ade2b715ca74f806725cc04f
Jim Vallandingham <vlandham@gmail.com>
2016-01-13 20:23:44 -0800

    First pass at a document list packager.

--- textkit/cli.py
+++ textkit/cli.py
@@ -4,6 +4,7 @@ from textkit.tokenize.sentences import text2sentences
 from textkit.filter.filter_punc import filterpunc
 from textkit.stats.count_tokens import count_tokens
 from textkit.transform.lowercase import lowercase
+from textkit.package.to_json import tokens2json
 
 
 @click.group()
@@ -17,3 +18,4 @@ cli.add_command(text2sentences)
 cli.add_command(filterpunc)
 cli.add_command(count_tokens)
 cli.add_command(lowercase)
+cli.add_command(tokens2json)
