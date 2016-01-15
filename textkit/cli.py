9591fd0767df277cd90432b15b71d03c32d1f9f2
Jim Vallandingham <vlandham@gmail.com>
2016-01-15 12:37:23 -0800

    Add top-bigrams collector. Probably shouldn’t be in stats.

--- textkit/cli.py
+++ textkit/cli.py
@@ -4,13 +4,14 @@ from textkit.tokenize.sentences import text2sentences
 from textkit.tokenize.bigrams import words2bigrams
 from textkit.tokenize.punc import text2punc
 from textkit.filter.filter_punc import filterpunc
-from textkit.stats.count_tokens import count_tokens
 from textkit.transform.lowercase import lowercase
 from textkit.transform.uppercase import uppercase
 from textkit.transform.newlines import nonewlines
 from textkit.package.tokens_to_json import tokens2json
 from textkit.package.texts_to_json import texts2json
 from textkit.package.tokens_to_text import tokens2text
+from textkit.stats.count_tokens import count_tokens
+from textkit.stats.top_bigrams import top_bigrams
 
 
 @click.group()
@@ -24,10 +25,11 @@ cli.add_command(text2sentences)
 cli.add_command(words2bigrams)
 cli.add_command(text2punc)
 cli.add_command(filterpunc)
-cli.add_command(count_tokens)
 cli.add_command(lowercase)
 cli.add_command(uppercase)
 cli.add_command(nonewlines)
 cli.add_command(tokens2json)
 cli.add_command(texts2json)
 cli.add_command(tokens2text)
+cli.add_command(count_tokens)
+cli.add_command(top_bigrams)
---
+++ textkit/stats/top_bigrams.py
@@ -0,0 +1,46 @@
+import click
+import nltk
+from textkit.utils import read_tokens, output
+
+
+measures = {
+    'likelihood': nltk.collocations.BigramAssocMeasures.likelihood_ratio,
+    'chi_sq': nltk.collocations.BigramAssocMeasures.chi_sq,
+    'pmi': nltk.collocations.BigramAssocMeasures.pmi,
+    'student_t': nltk.collocations.BigramAssocMeasures.student_t,
+    'freq': nltk.collocations.BigramAssocMeasures.raw_freq
+}
+
+
+@click.command('topbigrams')
+@click.argument('tokens', type=click.File('r'))
+@click.option('--sep', default=' ',
+              help='Separator between tokens and scores in output.',
+              show_default=True)
+@click.option('-m', '--measure', type=click.Choice(list(measures.keys())),
+              default=list(measures.keys())[0],
+              help='Specify which measure to use to define interesing-ness.',
+              show_default=True)
+@click.option('--freq', default=2,
+              help='Minimum frequency of bi-grams to filter out.',
+              show_default=True)
+@click.option('--scores/--no-scores', default=False,
+              help='Include or exclude scores in output.',
+              show_default=True)
+def top_bigrams(sep, measure, freq, scores, tokens):
+    '''Find top most interesting bi-grams in a token document.
+    Uses the --measure argument to determine what measure to use to define 'interesting'.
+    '''
+
+    output(sep)
+    content = read_tokens(tokens)
+    bcf = nltk.collocations.BigramCollocationFinder.from_words(content)
+    bcf.apply_freq_filter(freq)
+
+    nltk_measure = measures[measure]
+    bigrams = bcf.score_ngrams(nltk_measure)
+
+    out = [b[0] for b in bigrams]
+    if(scores):
+        out = [b[0] + tuple([str(b[1])]) for b in bigrams]
+    [output(sep.join(line)) for line in out]
