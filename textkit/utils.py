e468a83315b77ece1b3d43b57a6e19798337336e
Jim Vallandingham <vlandham@gmail.com>
2016-03-26 14:48:43 -0700

    Packaging system can now deal with token count input.
    Columns will be coerced into correct type
    (int, float, string) if possible.

--- textkit/utils.py
+++ textkit/utils.py
@@ -1,11 +1,20 @@
 import click
 import sys
+import csv
 
 
 def read_tokens(file):
     '''Reads tokens from a file handle'''
     lines = file.readlines()
     return [l.rstrip('\n') for l in lines if len(l.rstrip('\n')) > 0]
+
+
+def read_csv(file, delim):
+    '''Reads csv formated tokens from a file handle'''
+    lines = []
+    reader = csv.reader(file, delimiter=delim)
+    lines = [line for line in reader]
+    return lines
 
 
 def output(line):
