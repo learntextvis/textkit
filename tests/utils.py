8276966fc865e857a12e0da781413ac794ecbac5
Jim Vallandingham <vlandham@gmail.com>
2016-03-26 13:21:00 -0700

    convert tabs to spaces

--- tests/utils.py
+++ tests/utils.py
@@ -1,18 +1,20 @@
 def create_single_output(filename, content):
-	"""
-	Outputs test content into a filename
-	"""
-	with open(filename, 'w') as f:
-		f.write(content)
+    """
+    Outputs test content into a filename
+    """
+    with open(filename, 'w') as f:
+        f.write(content)
+
 
 def create_multifile_output(filenames, contents):
-	"""
-	Outputs several text contents into several files
-	"""
-	for idx, filename in enumerate(filenames):
-		with open(filename, 'w') as f:
-			f.write(contents[idx])
+    """
+    Outputs several text contents into several files
+    """
+    for idx, filename in enumerate(filenames):
+        with open(filename, 'w') as f:
+            f.write(contents[idx])
+
 
 def compare_results(tokens, expected_tokens):
-	for tdx, expected_token in enumerate(expected_tokens):
-		assert tokens[tdx] == expected_tokens[tdx]
+    for tdx, expected_token in enumerate(expected_tokens):
+        assert tokens[tdx] == expected_tokens[tdx]
