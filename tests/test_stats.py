cf1caa5d22c55e40d680e06556c7b9fd1a71b9bf
Jim Vallandingham <vlandham@gmail.com>
2016-04-09 14:45:48 -0700

    fix bad counts name

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,6 +1,6 @@
 import click
 from click.testing import CliRunner
-from textkit.stats.count_tokens import count_tokens
+from textkit.stats.tokens_to_counts import tokens2counts
 from textkit.stats.top_bigrams import top_bigrams
 from tests.utils import create_single_output, create_multifile_output, compare_results
 
@@ -14,7 +14,7 @@ def test_count_tokens():
                            'this,1', '\"Hello,\",1', '!,1', '']
         expected_tokens.sort()
         create_single_output(filename, sentence)
-        result = runner.invoke(count_tokens, [filename])
+        result = runner.invoke(tokens2counts, [filename])
         tokens = result.output.split('\n')
         tokens.sort()
         assert result.exit_code == 0
