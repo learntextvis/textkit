9b006737d20fb57dec354a94082b1109f6ddcd51
Alfred Lee <justalfred@users.noreply.github.com>
2016-04-24 16:58:40 -0400

    add wordnet lemmatizer as a stemming option

--- textkit/transform/stem.py
+++ textkit/transform/stem.py
@@ -2,13 +2,14 @@ import click
 from nltk.stem import PorterStemmer
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.stem.snowball import EnglishStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
 from textkit.utils import read_tokens, output
 
 
 @click.command()
 @click.argument('tokens', type=click.File('r'), default=click.open_file('-'))
 @click.option('--algorithm', default='porter',
-              help='Choice of stemming algorithm: porter, lancaster, snowball',
+              help='Choice of stemming algorithm: porter, lancaster, snowball, wordnet',
               type=click.STRING,
               show_default=True)
 def stem(tokens, algorithm):
@@ -18,7 +19,12 @@ def stem(tokens, algorithm):
     stemmer_classes = {'porter': PorterStemmer,
                        'lancaster': LancasterStemmer,
                        'snowball': EnglishStemmer}
-    if algorithm.lower() in stemmer_classes:
+    if algorithm.lower() == 'wordnet':
+        stemmer = WordNetLemmatizer()
+        for token in content:
+            output(stemmer.lemmatize(token))
+
+    elif algorithm.lower() in stemmer_classes:
         stemmer = stemmer_classes[algorithm.lower()]()
         for token in content:
             output(stemmer.stem(token))
